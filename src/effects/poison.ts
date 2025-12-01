669a4bcf35d408bc5cdb8ed766a8464b091aa05b
Curtis Erickson <incognitus.me@gmail.com>
2025-12-01 09:06:33 -0600

    feat(effects): added poison effect template
    - Created src/effects/poison.ts with poison DoT effect template
    - Includes onApply act message when poison is applied
    - Includes onTick act message with {damage} placeholder when poison ticks
    - Deals 10 damage per tick, 2 second interval, 12 second duration
    - Stackable and offensive effect

---
+++ src/effects/poison.ts
@@ -0,0 +1,30 @@
+/**
+ * Poison effect template - A damage over time effect that deals poison damage.
+ *
+ * This effect deals damage over time and includes act messages for when
+ * the poison is applied and when it ticks.
+ */
+
+import { DamageOverTimeEffectTemplate } from "../core/effect.js";
+
+export const EFFECT_TEMPLATE_ID = "poison";
+
+export const effectTemplate: DamageOverTimeEffectTemplate = {
+	id: EFFECT_TEMPLATE_ID,
+	name: "Poison",
+	description: "Deals damage over time from poison.",
+	type: "damage-over-time",
+	stackable: true,
+	damage: 10,
+	interval: 2, // 2 second interval
+	duration: 12, // 12 seconds total (6 ticks)
+	isOffensive: true,
+	onApply: {
+		user: "You feel a toxic poison coursing through your veins!",
+		room: "{User} has been poisoned!",
+	},
+	onTick: {
+		user: "You take {damage} poison damage!",
+		room: "{User} takes {damage} poison damage!",
+	},
+};

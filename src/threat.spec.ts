75d555528ce99a7f61b4be63f3b499c8a16326ca
Curtis Erickson <incognitus.me@gmail.com>
2025-12-17 00:57:07 -0600

    chore: moved threat tests

--- src/threat.spec.ts
+++ src/threat.spec.ts
@@ -1,11 +1,11 @@
 import { test, suite, beforeEach, skip, afterEach } from "node:test";
 import assert from "node:assert";
-import { greaterThanOrEqual } from "../utils/assert.js";
-import { Dungeon, Mob, Room, BEHAVIOR, ThreatEntry } from "../core/dungeon.js";
-import { Character } from "../core/character.js";
-import { processThreatSwitching } from "../registry/combat.js";
-import { freezeArchetype, Job, Race } from "../core/archetype.js";
-import { createMob } from "../package/dungeon.js";
+import { greaterThanOrEqual } from "./utils/assert.js";
+import { Dungeon, Mob, Room, BEHAVIOR, ThreatEntry } from "./core/dungeon.js";
+import { Character } from "./core/character.js";
+import { processThreatSwitching } from "./registry/combat.js";
+import { freezeArchetype, Job, Race } from "./core/archetype.js";
+import { createMob } from "./package/dungeon.js";
 
 const testRace: Race = {
 	id: "test_race",
